f111610e9e73a67e50a60a5fcc278ec6abc97028
Cass <cassianoleaoramos@gmail.com>
2024-07-02 20:03:01 -0300

    feat: create get units service

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GetUnitsService } from '../../services/get-units.service';
 
 @Component({
   selector: 'app-forms',
@@ -12,11 +13,16 @@ export class FormsComponent {
   results = [ReactiveFormsModule];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private getUnitsService: GetUnitsService
+  ) {
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: false,
     });
+
+    getUnitsService.getAllUnits().subscribe((data) => console.log(data));
   }
 
   onsubmit(): void {
